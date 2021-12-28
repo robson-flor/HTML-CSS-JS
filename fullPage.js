e0fc5f6a7b6a6a6897a1348bed4fe7d1e0fe6b12
robson-flor <robsonbuziosbr@outlook.com>
2021-12-27 21:38:04 -0300

    clique modal

--- fullPage.js
+++ fullPage.js
@@ -18,3 +18,22 @@ if(elementos.length){
     animaScroll();
   })
 }
+
+function cliqueModal(img) {
+  const janelaModal=document.getElementById('janelaModal');
+  const imgModal=document.getElementById('imgModal');
+  const txtModal=document.getElementById('txtModal');
+  const btnFechar = document.getElementById('btnFechar');
+  
+  janelaModal.classList.remove("escondeJanelaModal");
+  janelaModal.classList.add("mostraJanelaModal")
+
+  imgModal.src = "img.src";
+  imgModal.alt = "img.alt";
+  txtModal.innerHTML = img.alt;
+
+  btnFechar.onclick = function () {
+    janelaModal.classList.add("escondeJanelaModal")
+    janelaModal.classList.remove("mostraJanelaModal")
+  }
+}
