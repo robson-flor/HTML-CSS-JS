d726ce169f44d3fbde28bd3dff57cecf9dc02eb7
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 10:45:53 -0300

    retirado fizzbuzz

--- js/aula.js
+++ js/aula.js
@@ -195,27 +195,9 @@ let i = 0;
   //   return numero1;
   // else return numero2;
 
-//FIZZBUZZ
-//divisível por 3 = fizz
-//divisivel por 5 = buzz
-//divisivel por 3 e 5 = fizzbuzz
-//nao divisivel por 3 ou 5 = entrada
-//nao eh um numero = Nao eh um numero
 
-// const resultado = fizzBuzz(11);
-// console.log(resultado);
 
-// function fizzBuzz(entrada) {
-//   if (typeof entrada !== 'number')
-//     return 'Nao eh um numero';
-//     if (entrada % 3 === 0 && entrada % 5 === 0)
-//       return 'FizzBuzz';
-//   if (entrada % 3 === 0)
-//     return 'Fizz';
-//   if (entrada % 5 == 0)
-//     return 'Buzz';
-//   return entrada;
-// }
+
 
 
 
