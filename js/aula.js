6c20cf8dc8f0f64c03ad543e9b688791ae130683
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 09:37:03 -0300

    fizzbuzz

--- js/aula.js
+++ js/aula.js
@@ -183,14 +183,37 @@ let i = 0;
 
 // Escreva uma função que usa 2 números e retorna o maior entre eles
 
-let valorMaior = max(15, 19);
-console.log(valorMaior);
+// let valorMaior = max(15, 19);
+// console.log(valorMaior);
 
-function max(numero1, numero2) {
-  return numero1 > numero2 ? numero1 : numero2;
+// function max(numero1, numero2) {
+//   return numero1 > numero2 ? numero1 : numero2;
 
 
+// }
   // if (numero1 > numero2)
   //   return numero1;
   // else return numero2;
+
+//FIZZBUZZ
+//divisível por 3 = fizz
+//divisivel por 5 = buzz
+//divisivel por 3 e 5 = fizzbuzz
+//nao divisivel por 3 ou 5 = entrada
+//nao eh um numero = Nao eh um numero
+
+const resultado = fizzBuzz(11);
+console.log(resultado);
+
+function fizzBuzz(entrada) {
+  if (typeof entrada !== 'number')
+    return 'Nao eh um numero';
+    if (entrada % 3 === 0 && entrada % 5 === 0)
+      return 'FizzBuzz';
+  if (entrada % 3 === 0)
+    return 'Fizz';
+  if (entrada % 5 == 0)
+    return 'Buzz';
+  return entrada;
 }
+
