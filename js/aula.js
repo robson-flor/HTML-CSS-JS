ffb6ee7b55b76bf76074085c9f2e60748936a792
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 10:44:11 -0300

    retirado medidor de velocidade

--- js/aula.js
+++ js/aula.js
@@ -217,24 +217,5 @@ let i = 0;
 //   return entrada;
 // }
 
-// MEDIDOR DE VELOCIDADE
-// Velocidade maxima de ateh 70km/h
-//a cada 5km acima do limite voce ganha 1 ponto na carteira
-//Math.Floor() arredonda para o decimal mais proximo
-// caso os pontos sejam maior que 12 -> "Carteira Suspendida"
 
-// verificarVelocidade(130);
 
-// function verificarVelocidade(velocidade) {
-//   const velocidadeMaxima = 70;
-//   const kmPorPonto = 5;
-//   if (velocidade <= velocidadeMaxima)
-//     console.log('ok');
-//   else {
-//     const pontos = Math.floor(((velocidade - velocidadeMaxima) / kmPorPonto));
-//     if (pontos >= 12)
-//       console.log('Carteira Suspensa');
-//     else
-//       console.log('Pontos', pontos);
-//   }
-// }
