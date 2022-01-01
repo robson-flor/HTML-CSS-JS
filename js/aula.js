2bc58ae222614817d2e3595781e2a819b9b0c769
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 08:25:34 -0300

    js 1

--- js/aula.js
+++ js/aula.js
@@ -128,8 +128,41 @@
 
 //Laços --- FOR - while - do..while - for..in - for..of
 
-for (let i = 5; i >= 1; i--){
-  if (i % 2 !== 0) {
-    console.log(i);
-  }
+// for (let i = 5; i >= 1; i--){
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// //while loop
+// let i = 5;
+
+// while (i >= 1) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+//   i--;
+// }
+
+//do..while
+let i = 0;
+
+// do {
+//   console.log('digitando..');
+//   i++;
+// }while(i < 10)
+
+const pessoa = {
+  nome: 'Jhonatan',
+  idade: 25
+};
+
+for (let chave in pessoa) {
+  console.log(chave,pessoa.nome);
+}
+
+const cores = ['Vermelho', 'Azul', 'Verde']
+
+for (let indice in cores) {
+  console.log(indice,cores[indice])
 }
