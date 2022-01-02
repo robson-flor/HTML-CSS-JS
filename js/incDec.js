3f199e83af81379a8ee0e1a92e8ebbd9eceb03b8
robson-flor <robsonbuziosbr@outlook.com>
2022-01-02 20:30:32 -0300

    incremento e decremento

---
+++ js/incDec.js
@@ -0,0 +1,29 @@
+let num = 10;
+
+num++;//num=num+1;
+num--;//num=num-1;
+
+num *= -1;
+
+document.write(num);
+
+
+
+
+// Incremento e decremento de variáveis
+
+// num++;
+// num--;
+// num+=2;
+// num-=2;
+// num*=3;
+// num/=4;`
+
+// pós-incremento: incrementa o conteúdo da variável no final.
+
+// num++;
+
+
+// pré-incremento: incrementa o conteúdo da variável no início.
+
+// ++num;
