71adb32bb2518cd85d09ff68b867cb380bc1573e
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 21:47:11 -0300

    modified; retirado comentário

--- js/cfbJs.js
+++ js/cfbJs.js
@@ -1,37 +1,37 @@
-// let canal = "cfb cursos";
-// let curso = "curso de programação web";
-// const separador = " - ";
-// const quebra = "\n"
-// const info ="Entre com seu nome"
+let canal = "cfb cursos";
+let curso = "curso de programação web";
+const separador = " - ";
+const quebra = "\n"
+const info ="Entre com seu nome"
 
-// let nome = prompt("Digite seu nome", info);
-// alert(nome);
+let nome = prompt("Digite seu nome", info);
+alert(nome);
 
-// let nome = prompt ("Digite seu nome: ", "Nome aqui")
-// let res = confirm(nome +","+ " Deseja continuar?");
-// alert(res);
+let nome = prompt ("Digite seu nome: ", "Nome aqui")
+let res = confirm(nome +","+ " Deseja continuar?");
+alert(res);
 
-// let v1, v2, tx1, tx2;
+let v1, v2, tx1, tx2;
 
-// v1 = 10;
-// v2 = 10;
-// tx1 = "CFB - Cursos";
-// tx2 = "Curso de JavaScript";
+v1 = 10;
+v2 = 10;
+tx1 = "CFB - Cursos";
+tx2 = "Curso de JavaScript";
 
-// if (v1 != v2) {
-//   document.write(tx1);
-//   alert(tx1);
-// } else {
-//   document.write(tx2);
-//   alert(tx2);
-// }
+if (v1 != v2) {
+  document.write(tx1);
+  alert(tx1);
+} else {
+  document.write(tx2);
+  alert(tx2);
+}
 
-// let pontos = 40;
-// let energia = 50;
-// // vitoria --> >=30 && <=40 || >=70 && <=80
+let pontos = 40;
+let energia = 50;
+// vitoria --> >=30 && <=40 || >=70 && <=80
 
-// if ( ((pontos>=30)&&(pontos<=40))&&(energia>=40) || ((pontos>=70) && (pontos<=80)&&(energia>=40)) ){
-//   document.write("Ganhou");
-// } else {
-//   document.write("Perdeu");
-// }
+if ( ((pontos>=30)&&(pontos<=40))&&(energia>=40) || ((pontos>=70) && (pontos<=80)&&(energia>=40)) ){
+  document.write("Ganhou");
+} else {
+  document.write("Perdeu");
+}
