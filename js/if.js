589c54c2ff730e153eb72e145bd130b913774e60
robson-flor <robsonbuziosbr@outlook.com>
2022-01-02 10:35:26 -0300

    exemplo bloco de comando

---
+++ js/if.js
@@ -0,0 +1,19 @@
+let num = 10;
+
+
+if (num > 10) {
+  res = "Maior que 10";
+  document.write(res);
+  alert(res);
+} else {
+  res = "Menor que 10";
+  document.write(res);
+  alert(res);
+}
+
+
+// Blocos de comando { }
+
+// Delimita o início e o fim do bloco de comando {   }
+
+// O uso das chaves { } é obrigatório quando eu quiser delimitar um número maior do que um de instruções de um bloco.
