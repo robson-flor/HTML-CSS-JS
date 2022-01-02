b31ae5ba70b81578aa3b8175eb35569ce90e6118
robson-flor <robsonbuziosbr@outlook.com>
2022-01-01 21:41:49 -0300

    encontra string

---
+++ js/encontraString.js
@@ -0,0 +1,6 @@
+//criar um método para ler propriedades de um objeto e exibir somente as propriedades do tipo string que estão nesse objeto
+
+exibirPropriedades(filme);
+function exibirPropriedades(obj) {
+  
+}
