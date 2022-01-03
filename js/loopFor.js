613f94e59641e12267260686a0cfa9d2a911e6f9
robson-flor <robsonbuziosbr@outlook.com>
2022-01-03 08:37:53 -0300

    loop for

---
+++ js/loopFor.js
@@ -0,0 +1,26 @@
+
+//for com matriz
+let mochila = [["Corda",1],["Cura",10],["Lanterna",1],["Pilhas",20]];
+
+
+for (let l = 0; l < 4; l++){
+  for (let c = 0; c < 2; c++){
+    document.write(mochila[l][c] + " ");
+  }
+  document.write("<br>");
+}
+
+//for
+
+let num = [];
+
+num.push(0);
+num.push(1);
+num.push(2);
+num.push(3);
+num.push(4);
+num.push(5);
+
+for (contador = 0; contador < 5; contador++){
+  document.write(num[contador]+"<br>");
+}
