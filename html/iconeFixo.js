29fa30c29ca8b3131d69a8b2c7b2b25bb9b63045
robson-flor <robsonbuziosbr@outlook.com>
2022-01-07 22:35:55 -0300

    menu lateral

--- html/iconeFixo.js
+++ html/iconeFixo.js
@@ -1,10 +1,16 @@
-const menu = document.querySelectorAll('[data-menu]');
+const menus = document.querySelectorAll('[data-menu]');
 
-menu.forEach(function (e) {
+menus.forEach(function (e) {
   e.addEventListener('click', function () {
-    for (i = 0; i < menu.length; i++){
-      menu[i].classList.remove(cssAtivo);
+    for (i = 0; i < menus.length; i++){
+      menus[i].classList.remove(cssAtivo);
     }
     e.classList.add(cssAtivo);
     })
+});
+
+const btnMenu = document.getElementsByClassName('btnMenu')[0];
+const menu = getElementsByClassName('menu')[0];
+btnMenu.addEventListener('click', function () {
+  menu.classList.toggle('menuAberto');
 });
