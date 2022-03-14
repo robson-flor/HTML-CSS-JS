833cdeaf0d8fed35bb7ad1e548b7e05e95492b5c
robson-flor <robsonbuziosbr@outlook.com>
2022-03-13 23:19:36 -0300

    menu rolante que fixa no topo

---
+++ menuRolante/script.js
@@ -0,0 +1,13 @@
+const nav = document.getElementsByTagName("nav")[0];
+const topoNav = nav.offsetTop;
+
+window.onscroll = function () {
+  fixarMenuNoTopo();
+}
+function fixarMenuNoTopo() {
+  if (window.pageYOffset >= topoNav) {
+    nav.classList.add("FixoNoTopo");
+  } else {
+    nav.classList.remove("FixoNoTopo");
+  }
+}
